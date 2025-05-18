0b3da24f30b7d23f8dac429f186d934091da3166
playertk <playertk@163.com>
2025-05-19 06:31:02 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./ub/css/ueb-style.css";
+import { Blueprint } from "./ub/ueblueprint.js";
+
+export default function App() {
+  return (
+    <>
+      <ueb-blueprint>
+        Begin Object Class=/Script/UnrealEd.EdGraphNode_Comment
+        Name="EdGraphNode_Comment_1"
+        ExportPath="/Script/UnrealEd.EdGraphNode_Comment'/Game/Blueprints/BP_DevTest.BP_DevTest:EventGraph.EdGraphNode_Comment_1'"
+        CommentColor=(R=1.000000,G=0.700509,B=0.083688,A=1.000000)
+        bCommentBubbleVisible_InDetailsPanel=False NodePosX=-448 NodePosY=-224
+        NodeWidth=384 NodeHeight=48 bCommentBubblePinned=False
+        bCommentBubbleVisible=False NodeComment="🎯Test"
+        NodeGuid=39EA66BD466FB374C7EF058510301DDD End Object
+      </ueb-blueprint>
+    </>
+  );
+}
